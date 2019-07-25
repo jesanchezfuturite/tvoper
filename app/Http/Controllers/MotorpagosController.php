035db0ca11f06e64ae69fac90dc96d8fc122d4ee
jsanchez <jsanchez@MacBook-Air-de-jsanchez.local>
2019-07-25 12:06:55 -0500

    Cambiar la relacion de metodo de pago

--- app/Http/Controllers/MotorpagosController.php
+++ app/Http/Controllers/MotorpagosController.php
@@ -9,15 +9,23 @@ use Illuminate\Support\Facades\Auth;
 
 use Illuminate\Support\Facades\Log;
 
-
+use App\Repositories\EgobiernodiasferiadosRepositoryEloquent;
 
 class MotorpagosController extends Controller
 {
+    
+	protected $diasferiadosdb;
+
     // In this method we ensure that the user is logged in using the middleware
 
-    public function __construct()
+
+    public function __construct( 
+    	EgobiernodiasferiadosRepositoryEloquent $diasferiadosdb
+     )
     {
         $this->middleware('auth');
+
+        $this->diasferiadosdb = $diasferiadosdb;
     }
 
     /**
@@ -31,8 +39,27 @@ class MotorpagosController extends Controller
      */
     public function diasferiados()
     {
-    	
-    	return view('motorpagos/diasferiados');
+    	try
+    	{
+
+    		$info = $this->diasferiadosdb->all();
+
+    	}catch( \Exception $e ){
+    		Log::info('Error Method diasferiados: '.$e->getMessage());
+    	}
+
+    	$response = array();
+
+    	foreach($info as $i)
+    	{
+    		$response []= array(
+    			"anio" 	=> $i->Ano,
+    			"mes" 	=> $i->Mes,
+    			"dia"	=> $i->Dia
+    		);
+    	}
+
+    	return view('motorpagos/diasferiados', [ "saved_days" => $response ]);
     }
     /**
      * Muestra la vista para capturar nuevos metodos de pago y el listado de los que ya estan capturados
