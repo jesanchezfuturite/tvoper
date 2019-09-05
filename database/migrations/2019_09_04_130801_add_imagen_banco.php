b8d7051b619c29700f1242d49b27a4a084bb79ed
Claudia Romero Celis <claudia.romerocelis@genl.nl.gob>
2019-09-05 10:57:02 -0500

    guarda imagen en base de datos tipo base64, migracion campo imagen en tabla oper_banco

---
+++ database/migrations/2019_09_04_130801_add_imagen_banco.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddImagenBanco extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+       Schema::table('oper_banco', function (Blueprint $table) {
+            $table->longblob('imagen', 100)->after('nombre');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
