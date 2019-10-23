608956610de270ee3570b8c6dc38731846cd3f3e
Claudia Romero Celis <claudia.romerocelis@genl.nl.gob>
2019-10-23 12:34:29 -0500

    nuevo campo alias y vista consulta trmaites egob.

--- app/Repositories/EgobiernotransaccionesRepositoryEloquent.php
+++ app/Repositories/EgobiernotransaccionesRepositoryEloquent.php
@@ -62,5 +62,26 @@ class EgobiernotransaccionesRepositoryEloquent extends BaseRepository implements
             return false;
         }
     }
+    public function consultaTransacciones($fechaIn,$fechaF)
+    {
+        try
+       { 
+               
+            $data = Egobiernotransacciones::whereBetween('fechatramite',[$fechaIn,$fechaF])
+            ->join('status','status.Status','=','transacciones.Status')
+            ->join('operacion.oper_entidadtramite','operacion.oper_entidadtramite.tipo_servicios_id','=','transacciones.TipoServicio')
+            ->join('operacion.oper_entidad','operacion.oper_entidad.id','=','operacion.oper_entidadtramite.entidad_id')
+            ->join('tipo_servicios','tipo_servicios.Tipo_Code', '=','transacciones.TipoServicio')
+            ->join('tipopago','tipopago.TipoPago', '=','transacciones.TipoPago')
+            ->select('status.Descripcion as status','transacciones.idTrans','operacion.oper_entidad.nombre as entidad','tipo_servicios.Tipo_Descripcion as tiposervicio','transacciones.TitularTC','transacciones.fechatramite','transacciones.HoraTramite','transacciones.BancoSeleccion','tipopago.Descripcion as tipopago','transacciones.TotalTramite')
+            ->groupBy('transacciones.idTrans')
+            ->get();
+            return $data;
+            //Log::info($data);
+        }catch( \Exception $e ){
+            Log::info("[EgobiernotransaccionesRepositoryEloquent @ consultaTransacciones]  ERROR al actualizar las transacciones como procesadas en egobierno");
+            return false;
+        }
+    }
     
 }
