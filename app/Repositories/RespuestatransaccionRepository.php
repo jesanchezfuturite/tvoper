85daf943c9db17052abe8a8f2cf0df2b51ea46d8
Claudia Romero Celis <claudia.romerocelis@genl.nl.gob>
2020-02-25 14:00:05 -0600

    conexion a QA

---
+++ app/Repositories/RespuestatransaccionRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Repositories;
+
+use Prettus\Repository\Contracts\RepositoryInterface;
+
+/**
+ * Interface RespuestatransaccionRepository.
+ *
+ * @package namespace App\Repositories;
+ */
+interface RespuestatransaccionRepository extends RepositoryInterface
+{
+    //
+}
