4c7b9968f59184544b8bc358de232cc76b9863a7
Claudia Romero Celis <claudia.romerocelis@genl.nl.gob>
2020-04-08 18:07:16 -0500

    soap

--- config/app.php
+++ config/app.php
@@ -175,7 +175,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Artisaninweb\SoapWrapper\ServiceProvider::class,
+        //Artisaninweb\SoapWrapper\ServiceProvider::class,
 
     ],
 
@@ -227,7 +227,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'SoapWrapper' => Artisaninweb\SoapWrapper\Facade\SoapWrapper::class,
+        //'SoapWrapper' => Artisaninweb\SoapWrapper\Facade\SoapWrapper::class,
 
     ],
 
