a420a447ac9012b963e21172d4c78c6caa7618ab
jsanchez <jsanchez@MacBook-Air-de-jsanchez.local>
2020-08-31 13:05:27 -0500

    Database layers

---
+++ app/Entities/Portalcamporelationship.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Prettus\Repository\Contracts\Transformable;
+use Prettus\Repository\Traits\TransformableTrait;
+
+/**
+ * Class Portalcamporelationship.
+ *
+ * @package namespace App\Entities;
+ */
+class Portalcamporelationship extends Model implements Transformable
+{
+    use TransformableTrait;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $connection = "mysql6";
+
+    protected $fillable = ['tramite_id','campo_id','tipo_id','caracteristicas'];
+
+    protected $table = "campos_relationship";
+
+}
