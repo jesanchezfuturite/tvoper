12e03dc82d97821c86c638355e8a71a89d1ab700
FECD <cerdaduran@gmail.com>
2020-09-23 23:58:47 -0500

    Controller, repositories and migration for masterlog

---
+++ app/Entities/Pagosupdatelog.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Prettus\Repository\Contracts\Transformable;
+use Prettus\Repository\Traits\TransformableTrait;
+
+/**
+ * Class Pagosupdatelog.
+ *
+ * @package namespace App\Entities;
+ */
+class Pagosupdatelog extends Model implements Transformable
+{
+    use TransformableTrait;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+
+
+    protected $table = "oper_pagosupdate_log";
+
+    protected $fillable = ['fecha_pago','referencia','id_transaccion','banco_desc','plataforma','monto','data'];
+
+}
