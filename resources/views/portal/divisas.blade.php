27010895b7f5ba0be30b5a49c031f62e37e617d4
Alberto <alberto.martinezve@famsa.com>
2020-11-25 11:49:58 -0600

    ajustes diseño

--- resources/views/portal/divisas.blade.php
+++ resources/views/portal/divisas.blade.php
@@ -48,182 +48,39 @@
             <div class="portlet-title">
 
                 <div class="caption" id="headerTabla">
-                    <i class="fa fa-cogs"></i>&nbsp;<span id="nameTramite">Tramite</span> &nbsp;
+                    <i class="fa fa-cogs"></i>&nbsp;<span id="divisas">Divisas</span> &nbsp;
                     <!--    <span class="label label-sm label-danger">
                             <label id='changeStatus'>No found</label>
                         </span>&nbsp;&nbsp;&nbsp;&nbsp;
                         --->
 
                 </div>
-                <div class="tools" id="removeBanco">
+                <div class="tools" id="divisasTools">
+                    <!--
                     <a href="#portlet-config" data-toggle="modal" class="config" data-original-title="" title="Agregar Cuenta">
                     </a>
                    <a id="Remov" href="javascript:;" data-original-title="" title="Desactivar Tramite" onclick="desactivaTramite()"><i class='fa fa-remove' style="color:white !important;"></i>
-                    </a>
+                    </a>-->
                 </div>
             </div>
             <div class="portlet-body" id="Removetable">
-                <div id="addtable">
-                    <table class="table table-hover" id="sample_2">
-                    <thead>
-                    <tr>
-                        <th>Tramite</th>
-                        <th>Campo</th>
-                        <th>Tipo</th>
-                        <th>Caracteristicas</th>
-                        <th></th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    </tbody>
-                    </table>
+                <div class="form-body">
+                    <div class="row">
+                        <div class="col-md-4 col-ms-12">
+                            <div class="form-group">
+                                <select class="select2me form-control" name="divisas" id="divisas">
+                                    <option value="limpia">------</option>
+
+                                </select>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
         <!-- END SAMPLE TABLE PORTLET-->
 
 </div>
-<div id="modaldelete" class="modal fade " tabindex="-1" data-backdrop="static" data-keyboard="false">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true" onclick="CleanInputs()"></button>
-                <h4 class="modal-title">Confirmation</h4>
-            </div>
-            <div class="modal-body">
-                <p>
-             ¿Eliminar Registro?
-                </p>
-                 <input hidden="true" type="text" name="iddeleted" id="iddeleted" class="iddeleted">
-            </div>
-            <div class="modal-footer">
-         <button type="button" data-dismiss="modal" class="btn default" onclick="CleanInputs()">Cancelar</button>
-            <button type="button" data-dismiss="modal" class="btn green" onclick="deleted()">Confirmar</button>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="modal fade" id="portlet-config" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true" onclick="CleanInputs()"></button>
-                <h4 class="modal-title">Configurar Tramite</h4>
-            </div>
-            <div class="modal-body">
-                    <div class="form-body">
-                        <input hidden="true" type="text"  placeholder="Ingrese una Cuenta" id="idRelantion">
-                        <div class="modal-body">
-                         <input hidden="true" type="text"  id="idupdate">
-                        <div class="row">
-                            <div class="col-md-12">
-                                <div class="col-md-8">
-                                    <div class="form-group">
-                                        <label >Campo</label>
-                                        <select class="select2me form-control"  id="itemsCampos">
-                                            <option>------</option>
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-12">
-                                <div class="col-md-8">
-                                    <div class="form-group">
-                                        <label>Tipo</label>
-                                        <select class="select2me form-control"  id="itemsTipos">
-                                            <option>------</option>
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-12">
-                                <div class='col-md-4'>
-                                    <div class='form-group'>
-                                        <label >Caracteristicas</label>
-                                        <div class='md-checkbox'>
-                                            <input type='checkbox' id='checkbox30' class='md-check'>
-                                            <label for='checkbox30'>
-                                                <span></span>
-                                                <span class='check'></span> <span class='box'>
-                                            </span>  Requerido. </label>
-                                        </div>
-                                        <span class='help-block'>Marque</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                    <div class="form-group">
-                        <div class="col-md-10">
-                            <button type="button" class="btn blue" onclick="metodoSaveUpdate()"><i class="fa fa-check"></i> Guardar</button>
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" data-dismiss="modal" class="btn default" onclick="CleanInputs()">Cerrar</button>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-        <!-- /.modal-content -->
-    </div>
-    <!-- /.modal-dialog -->
-</div>
-</div>
-<div class="modal fade" id="modalCaracteristica" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-        <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-hidden="true" onclick="CleanInputs()"></button>
-            <h4 class="modal-title">Caracteristicas del campo</h4>
-        </div>
-        <div class="modal-body">
-            <div class="form-body">
-                <input type="hidden" id="idcampo" >
-                <div class="modal-body">
-                  <input hidden="true" type="text"  id="idAdd">
-                  <div class="row">
-                        <div class="col-md-12">
-                            <div class="col-md-8">
-                                <div class="form-group">
-                                    <label >Nombre</label>
-                                    <input type="text" name="nombre" id="nombre" class="form-control" >
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                  <div class="row">
-                        <div class="col-md-12">
-                            <div class="col-md-8">
-                                <div class="form-group">
-                                    <label>Value</label>
-                                    <input type="text" name="valor" id="valor" class="form-control">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                  <div class="form-group">
-                      <div class="col-md-10">
-                          <button type="button" class="btn blue" onclick="saveCaracteristica()"><i class="fa fa-check"></i> Guardar</button>
-                      </div>
-                  </div>
-                  <div class="modal-footer">
-                      <button type="button" data-dismiss="modal" class="btn default" onclick="CleanInputs()">Cerrar</button>
-                  </div>
-                </div>
-            </div>
-        </div>
-      <!-- /.modal-content -->
-    </div>
-    <!-- /.modal-dialog -->
-  </div>
-</div>
-
-
 @endsection
 
 @section('scripts')
