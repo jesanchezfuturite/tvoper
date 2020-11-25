0c7546f931912af8e19963256dedeb4c457cf76c
karlaces <karla.ces@outlook.com>
2020-11-25 11:44:59 -0600

    guardar y borrar divisas

--- app/Http/Controllers/DivisasController.php
+++ app/Http/Controllers/DivisasController.php
@@ -27,6 +27,52 @@ class DivisasController extends Controller
         return $listDivisas;
        }
        public function saveDivisas(Request $request){
+        $data = $request->all();       
+           
+        $json=json_encode($data);
+        $ch = curl_init();
 
+        curl_setopt($ch, CURLOPT_URL,"https://session-api-pr-21.herokuapp.com/divisas/saveDivisas");
+        curl_setopt($ch, CURLOPT_POST, TRUE);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+        
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $remote_server_output = curl_exec($ch);
+        curl_close ($ch);
+        $response =$remote_server_output;
+    
+        return json_encode($response);
+         
        }
+
+       public function deleteDivisas(Request $request){
+        $data = $request->all();       
+           
+        $json=json_encode($data);
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL,"https://session-api-pr-21.herokuapp.com/divisas/deleteDivisas");
+        curl_setopt($ch, CURLOPT_POST, TRUE);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+        
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $remote_server_output = curl_exec($ch);
+        curl_close ($ch);
+        $response =$remote_server_output;
+    
+        return json_encode($response);
+         
+       }
+       public function getDivisasSave(){
+        $ch = curl_init();    
+        curl_setopt($ch, CURLOPT_URL,"https://session-api-pr-21.herokuapp.com/divisas/getDivisasSave"); 
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $getDivisas = curl_exec($ch);
+
+        curl_close($ch);  
+        return $getDivisas; 
+       }
+
 }
