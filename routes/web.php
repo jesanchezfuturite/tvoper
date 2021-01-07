bfbdb67a9bed9f2763f8657e00b86a449732b809
karlaces <karla.ces@outlook.com>
2021-01-07 15:31:51 -0600

    cambios en la funcion de update status

--- routes/web.php
+++ routes/web.php
@@ -375,6 +375,8 @@ Route::post('/cerrar-ticket', 'PortalSolicitudesController@cerrarTicket');
 Route::get('/listado-mensajes/{id}', 'PortalSolicitudesController@getMensajes');
 Route::get('/listado-download/{file}' , 'PortalSolicitudesController@downloadFile');
 Route::get('/get-route/{id}/{type}' , 'PortalSolicitudesController@getFileRoute');
+Route::post('/solicitudes-update-status' , 'PortalSolicitudesController@updateStatus');
+
 
 
 Route::post('/solicitudes-register', 'PortalSolicitudesTicketController@registrarSolicitud')->name("RegistrarSolicitud");
