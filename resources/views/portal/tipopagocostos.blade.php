ce482ad20455ba9cd0bd18a4dbf290c14e07a122
Claudia Romero Celis <claudia.romerocelis@genl.nl.gob>
2021-02-05 16:08:14 -0600

    modal subsidio

--- resources/views/portal/tipopagocostos.blade.php
+++ resources/views/portal/tipopagocostos.blade.php
@@ -270,8 +270,8 @@
              </div>
             </div>
           </div>
-        </div>
-      </div>-------->
+        </div>-------->
+      </div>
       <div class="row">
         <div class="col-md-12">
           <div class="col-md-12">
