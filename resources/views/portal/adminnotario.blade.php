5b72ec7066fe587025d6d7662b6c1649866f30a6
Claudia Romero Celis <claudia.romerocelis@genl.nl.gob>
2021-03-22 16:00:54 -0600

    campo calle minimo 3 caracteres.

--- resources/views/portal/adminnotario.blade.php
+++ resources/views/portal/adminnotario.blade.php
@@ -977,8 +977,8 @@ function changeComunidad()
       Command: toastr.warning("Campo Entidad Federativa, requerido!", "Notifications")
     }else if(itemsCiudadNot=="0"){
       Command: toastr.warning("Campo Ciudad, requerido!", "Notifications")
-    }else if (calleNotario.length<1) {
-       Command: toastr.warning("Campo Calle, requerido!", "Notifications") 
+    }else if (calleNotario.length<3) {
+       Command: toastr.warning("Campo Calle min. 3 caracteres, requerido!", "Notifications") 
        $("#calleNotario").focus(); 
     }else if (numeroExtNotario.length<1) {
        Command: toastr.warning("Campo Número Exterior, requerido!", "Notifications")
@@ -1098,7 +1098,7 @@ function changeComunidad()
     }else if(itemsCiudadNot=="0"){
       Command: toastr.warning("Campo Ciudad, requerido!", "Notifications")
       //$("#itemsCiudadNot").focus(); 
-    }else if (calleNotario.length<2) {
+    }else if (calleNotario.length<3) {
        Command: toastr.warning("Campo Calle min. 3 caracteres, requerido!", "Notifications") 
        $("#calleNotario").focus(); 
     }else if (numeroExtNotario.length<1) {
