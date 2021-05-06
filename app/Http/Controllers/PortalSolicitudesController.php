4a5f7af63507d80628276019d989d5e09cf5de0d
karlaces <karla.ces@outlook.com>
2021-05-06 10:40:06 -0500

    filtrar solicitudes

--- app/Http/Controllers/PortalSolicitudesController.php
+++ app/Http/Controllers/PortalSolicitudesController.php
@@ -467,7 +467,7 @@ class PortalSolicitudesController extends Controller
 
     }
     $solicitudes->where('solicitudes_ticket.status', '!=', 99)
-    // ->whereNull('solicitudes_ticket.asignado_a')
+    ->whereNull('solicitudes_ticket.asignado_a')
     ->where('solicitudes_ticket.asignado_a', $user_id)
     ->whereNotNull('solicitudes_ticket.id_transaccion')
     ->orderBy('solicitudes_ticket.created_at', 'DESC');
