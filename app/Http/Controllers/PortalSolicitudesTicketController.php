03881870c4548bad2f2c8ff15dce30f916354f9a
karlaces <karla.ces@outlook.com>
2021-05-21 16:05:58 -0500

    validacion del distrito 1 agregar logs

--- app/Http/Controllers/PortalSolicitudesTicketController.php
+++ app/Http/Controllers/PortalSolicitudesTicketController.php
@@ -692,11 +692,12 @@ class PortalSolicitudesTicketController extends Controller
       
               }
 
-           }else{
+          }else{
+            log::info("else si no hay campos configurados");
             if($value->status<>5){
               $tramites_finalizados = $this->tramites_finalizados($value->id);
             }
-           }
+          }
          
         }
 
@@ -706,6 +707,7 @@ class PortalSolicitudesTicketController extends Controller
           $error = $e;
       }
       if ($error) {
+        log::info("error PortalSolicitudesTicketController@statusTramite" .$e->getMessage());
         return response()->json(
           [
             "Code" => "400",
