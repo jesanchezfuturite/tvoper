a5f42174291683b418567ec34ba8a9190211b889
karlaces <karla.ces@outlook.com>
2021-05-28 12:06:22 -0500

    repository eloquent

--- app/Repositories/RegistradoresRepositoryEloquent.php
+++ app/Repositories/RegistradoresRepositoryEloquent.php
@@ -4,7 +4,7 @@ namespace App\Repositories;
 
 use Prettus\Repository\Eloquent\BaseRepository;
 use Prettus\Repository\Criteria\RequestCriteria;
-use App\Repositories\registradoresRepository;
+use App\Repositories\RegistradoresRepository;
 use App\Entities\Registradores;
 use App\Validators\RegistradoresValidator;
 
