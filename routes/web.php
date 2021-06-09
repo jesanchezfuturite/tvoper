54e7ebe2c8603dadc0f269d2d99715128c489675
karlaces <karla.ces@outlook.com>
2021-06-09 15:58:34 -0500

    agregar rutas

--- routes/web.php
+++ routes/web.php
@@ -484,4 +484,12 @@ Route::group(["prefix" => getenv("APP_PREFIX") ?? "/"], function(){
 
 
     Route::get('/campo-alias-update','PortalSolicitudesTicketController@updateAlias');
+
+    Route::get('/wsrc-individuo/{nombre}/{apaterno}/{amaterno}/{fechanac}','ApircController@buscarIndividuo');
+    Route::get('/wsrc-actanac/{nombre}/{apaterno}/{amaterno}/{fechanac}','ApircController@buscarActaNac');
+    Route::get('/wsrc-actadef/{nombre}/{apaterno}/{amaterno}/{genero}/{fechanac}','ApircController@buscarActaDef');
+    Route::get('/wsrc-actamat/{nombre}/{apaterno}/{amaterno}/{fechanac}','ApircController@buscarActaMat');
+
+    Route::get('/configprelacion','PortalSolicitudesController@configdocprelacion');
+    Route::post('/update-rechazo','PortalSolicitudesController@upStatusRechazo');
 });
