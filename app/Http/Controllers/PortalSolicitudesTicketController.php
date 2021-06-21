4d893b2bf7f3be903a8ef5a0db238df6c2c03f72
karlaces <karla.ces@outlook.com>
2021-06-21 11:52:57 -0500

    quitar valores seteados

--- app/Http/Controllers/PortalSolicitudesTicketController.php
+++ app/Http/Controllers/PortalSolicitudesTicketController.php
@@ -1778,11 +1778,9 @@ class PortalSolicitudesTicketController extends Controller
         $data = array(
           "referencia"=>$referencia
         );
-        //  $token = env("PAYMENTS_KEY");
-         $token= "yf3puRWCxfgV9kTTg9xK8mmo74QAhatjtvN2662RUrfC9WVaH7RGD7yUFJQyNF22JJvdhibXKv7kc298wLKtEYd39H9mfijq6XLk";  
+         $token = env("PAYMENTS_KEY");
          header('Content-Type: application/json'); 
-        //  $url =  env("PAYMENTS_HOSTNAME")."/v1/cancel";
-         $ch = curl_init("http://10.153.144.218/payments-api/v1/cancel"); 
+         $url =  env("PAYMENTS_HOSTNAME")."/v1/cancel";
          $ch = curl_init($url); 
          $post = json_encode($data); 
          $authorization = "Authorization: Bearer ".$token; 
