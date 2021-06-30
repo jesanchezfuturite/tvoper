52830abdba3730b043fd82fcfc230089a34a31b5
karlaces <karla.ces@outlook.com>
2021-06-30 10:22:48 -0500

    quitar coma en monto_operacion_ae

--- resources/views/motorpagos/consultatramites.blade.php
+++ resources/views/motorpagos/consultatramites.blade.php
@@ -1280,7 +1280,7 @@
                                 }
                             }
                             
-                            ).filter(Boolean).join(", ");
+                            ).filter(Boolean).join(" ");
 
                             obj.monto_operacion_ae=result;
                             
