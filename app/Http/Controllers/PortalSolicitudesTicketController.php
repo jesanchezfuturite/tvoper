c7d5246462a51fcf66506e5c9b8fea56a31c5f4a
karlaces <karla.ces@outlook.com>
2021-07-01 17:26:06 -0500

    quitar dd

--- app/Http/Controllers/PortalSolicitudesTicketController.php
+++ app/Http/Controllers/PortalSolicitudesTicketController.php
@@ -1854,7 +1854,6 @@ class PortalSolicitudesTicketController extends Controller
       }
   }
   public function addFolios(Request $request){
-    dd($request->all());
     $ids = $request->id; 
     $expedientes = $request->expedientes;
     $folios = $request->folios;
