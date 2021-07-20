57cd689ebd43d52ad55ef56c3451adc86197451e
Raymundo Salazar <raymundo.salser@hotmail.com>
2021-07-20 10:23:43 -0500

    use route method

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,7 +8,7 @@
                 <div class="card-header"></div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
+                    <form method="POST" action="{{ url()->route('register') }}">
                         @csrf
 
                         <div class="form-group row">
