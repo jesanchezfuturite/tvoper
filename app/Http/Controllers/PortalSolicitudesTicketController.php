c147b67bd28522ca2108647ff24cdfdf839849e0
karlaces <karla.ces@outlook.com>
2021-08-16 15:20:26 -0500

    agregar order by a obtener  archivos

--- app/Http/Controllers/PortalSolicitudesTicketController.php
+++ app/Http/Controllers/PortalSolicitudesTicketController.php
@@ -771,7 +771,7 @@ class PortalSolicitudesTicketController extends Controller
     public function getRegistroTramite($id){
       try {
         $solicitud = PortalSolicitudesTicket::with(["archivos" => function( $query ){
-          $query->where('status', 1);
+          $query->where('status', 1)->orderBy("id","DESC");
 
          }])->where('clave' , $id)->get();
 
