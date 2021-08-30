5babb87c3975b9191acc82ab0d8666fb120fd464
Raymundo Salazar <raymundo.salser@hotmail.com>
2021-08-30 16:09:39 -0500

    solicitudes-guardar-carrito

--- app/Http/Controllers/PortalSolicitudesTicketController.php
+++ app/Http/Controllers/PortalSolicitudesTicketController.php
@@ -36,7 +36,7 @@ use App\Repositories\OperacionUsuariosEstatusRepositoryEloquent;
 use Luecano\NumeroALetras\NumeroALetras;
 use Milon\Barcode\DNS1D;
 
-class PortalSolicitudesController extends Controller
+class PortalSolicitudesTicketController extends Controller
 {
   protected $users;
   protected $solicitudes;
