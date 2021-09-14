a65c57fba754c29c8eab43218575e96bb4e50097
karlaces <karla.ces@outlook.com>
2021-09-14 15:28:20 -0500

    agregar dd

--- app/Http/Controllers/PortalSolicitudesTicketController.php
+++ app/Http/Controllers/PortalSolicitudesTicketController.php
@@ -91,6 +91,7 @@ class PortalSolicitudesTicketController extends Controller
     public function registrarSolicitud(Request $request){  
       $name= \Request::route()->getName();
       $aviso=env("TRAMITE_AVISO");
+      dd($aviso, $request->catalogo_id);
       $status="";
       $token=null;
       if($name=="solicitudes-register-temporal"){
