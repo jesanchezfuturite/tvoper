30a2a7941cf5771ad547bb7d1485f8787c6170bd
karlaces <karla.ces@outlook.com>
2021-10-28 15:01:17 -0500

    agregar nombre mes

--- app/Http/Controllers/ConciliacionReporteController.php
+++ app/Http/Controllers/ConciliacionReporteController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Entities\Processedregisters;
+use DB;
 
 class ConciliacionReporteController extends Controller
 {
@@ -13,7 +14,21 @@ class ConciliacionReporteController extends Controller
     }
 
     public function getMonth(){
-        $month = Processedregisters::select("month")->distinct()->orderBy("month")->get()->toArray();
+        $month = Processedregisters::select("month", DB::raw('(CASE 
+        WHEN month = 1 THEN "Enero" 
+        WHEN month = 2 THEN "Febrero"
+        WHEN month = 3 THEN "Marzo" 
+        WHEN month = 4 THEN "Abril" 
+        WHEN month = 5 THEN "Mayo" 
+        WHEN month = 6 THEN "Junio" 
+        WHEN month = 7 THEN "Julio" 
+        WHEN month = 8 THEN "Agosto" 
+        WHEN month = 9 THEN "Septiembre" 
+        WHEN month = 10 THEN "Octubre" 
+        WHEN month = 11 THEN "Noviembre" 
+        WHEN month = 12 THEN "Diciembre"  
+        END) AS month'))
+        ->distinct()->orderBy("month")->get()->toArray();
         return $month;
     }
 
